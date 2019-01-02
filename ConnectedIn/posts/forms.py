0fe7490dbf8f5e145618d95dccb79a55ee543838
Firmo Lopes de Oliveira Neto <azurefirmo@live.com>
2019-01-02 20:47:46 -0300

    Avaliação: Feedback ao Usuário

--- ConnectedIn/posts/forms.py
+++ ConnectedIn/posts/forms.py
@@ -1,11 +1,19 @@
-from django.forms import ModelForm 
 from django import forms
+from django.forms import ModelForm
 from posts.models import *
 
+
 class PostModelForm(forms.ModelForm):
-	class Meta:
-		model = Post
-		fields = ['postagem']
-		widgets={
-			'postagem':forms.Textarea(attrs={'class': 'form-control', 'maxlength':255, 'rows':'4',  'cols':'20'}),
-		}
+    class Meta:
+        model = Post
+        fields = ['postagem']
+        widgets = {
+            'postagem': forms.Textarea(attrs={'class': 'form-control', 'maxlength': 255, 'rows': '4', 'cols': '20'}),
+        }
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['message']
+        labels = {'message': ""}
